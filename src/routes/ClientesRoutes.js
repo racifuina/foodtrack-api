2c9f6763b15dd48e616f7d14dcf7854ceb8dd58d
Rigoberto Acifuina <racifuina@me.com>
2022-10-07 21:44:00 -0600

    add axios and tax integration

--- src/routes/ClientesRoutes.js
+++ src/routes/ClientesRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAll, create, getById, updateById, deleteById } from '../controllers/ClientesController';
+import { getAll, create, getById, updateById, deleteById, validarNit } from '../controllers/ClientesController';
 import isAuth from '../middlewares/Auth';
 
 const routes = Router();
 
 routes.get('/', isAuth, getAll);
+routes.get('/validar-nit/:nitCliente', isAuth, validarNit);
 routes.post('/', isAuth, create);
 routes.get('/:id', isAuth, getById);
 routes.post('/:id', isAuth, updateById);
